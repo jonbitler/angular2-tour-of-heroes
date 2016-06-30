c2fd5f04814f908de6c7cd1a8e72594b8706d639
jonbitler <jbitler@jons-mbp.atlanta.beckshybrids.com>
2016-06-30 13:08:09 -0400

    gets first hero displaying and can change name on input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,27 @@
 import { Component } from '@angular/core';
-import { ROUTER_DIRECTIVES, Router } from '@angular/router';
 
-import { APP_PROVIDERS } from './app.providers';
+export class Hero {
+  id: number;
+  name: string;
+}
 
 @Component({ 
-  selector: 'app-container',
-  template: `<router-outlet></router-outlet>`,
-  directives: [ ROUTER_DIRECTIVES ],
-  providers: [ APP_PROVIDERS ]
+  selector: 'my-app',
+  template:  `<h1>{{title}}</h1>
+              <h2>{{hero.name}} details!</h2>
+              <div><label>id: </label>{{hero.id}}</div>
+              <div>
+                <label>name: </label>
+                <input [(ngModel)]="hero.name" placeholder="name" />
+              </div> 
+              `
 })
-export class AppComponent {
-  
-  constructor() {
 
-  }
+export class AppComponent {
+  title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
   
 }
